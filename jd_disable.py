2a3774393327bdcc6361f4d0a3c5c9e48cbac941
akali5 <2889034185@qq.com>
2022-07-14 17:51:46 +0800

    upudate

--- jd_disable.py
+++ jd_disable.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "===4===_KR")
+sub_str = os.getenv("RES_SUB", "===4===_jd-depot")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
