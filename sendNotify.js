a959ca11dd956390f1c57c7923f28d108796418a
akali5 <2889034185@qq.com>
2022-07-23 20:31:32 +0800

    新增：[ jd_opencardL201.js] 修改内容：[ sendNotify.js]

--- sendNotify.js
+++ sendNotify.js
@@ -1,37 +1,27 @@
 /*
- * sendNotify 推送通知功能
- * @param text 通知头
- * @param desp 通知体
- * @param params 某些推送通知方式点击弹窗可跳转, 例：{ url: 'https://abc.com' }
- * @param author 作者仓库等信息  例：`本通知 By：https://github.com/whyour/qinglong`
- 部分变量设置
-## 拆分通知
-export BEANCHANGE_PERSENT="10"
-## 如果通知标题在此变量里面存在(&隔开),则用屏蔽不发送通知
-export NOTIFY_SKIP_LIST="京东CK检测&京东资产变动"
-## 当接收到发送CK失效通知和Ninja 运行通知时候执行子线程任务
-export NOTIFY_CKTASK="jd_CheckCK.js"
-## 如果此变量(&隔开)的关键字在通知内容里面存在,则屏蔽不发送通知.
-export NOTIFY_SKIP_TEXT="忘了种植&异常"
-## 屏蔽任务脚本的ck失效通知
-export NOTIFY_NOCKFALSE="true"
-## 服务器空数据等错误不触发通知
-export CKNOWARNERROR="true"
-## 屏蔽青龙登陆成功通知，登陆失败不屏蔽
-export NOTIFY_NOLOGINSUCCESS="true"
-## 通知底部显示
-export NOTIFY_AUTHOR="来源于：https://github.com/Akali5/jd-depot"
-## 增加NOTIFY_AUTHOR_BLANK 环境变量，控制不显示底部信息
-export NOTIFY_AUTHOR_BLANK="true"
-## 增加NOTIFY_AUTOCHECKCK为true才开启通知脚本内置的自动禁用过期ck
-export NOTIFY_AUTOCHECKCK=“true”
+ * @Author: ccwav https://github.com/ccwav/QLScript2 
+ 
+ * sendNotify 推送通知功能 (text, desp, params , author , strsummary)
+ * @param text 通知标题  (必要)
+ * @param desp 通知内容  (必要)
+ * @param params 某些推送通知方式点击弹窗可跳转, 例：{ url: 'https://abc.com' } ，没啥用,只是为了兼容旧脚本保留  (非必要)
+ * @param author 通知底部作者`  (非必要)
+ * @param strsummary 指定某些微信模板通知的预览信息，空则默认为desp  (非必要)
+ 
+ * sendNotifybyWxPucher 一对一推送通知功能 (text, desp, PtPin, author, strsummary )
+ * @param text 通知标题  (必要)
+ * @param desp 通知内容  (必要)
+ * @param PtPin CK的PTPIN (必要)
+ * @param author 通知底部作者`  (非必要)
+ * @param strsummary 指定某些微信模板通知的预览信息，空则默认为desp  (非必要)
+ 
  */
 //详细说明参考 https://github.com/ccwav/QLScript2.
 const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("加载sendNotify，当前版本: 20220722");
+console.log("加载sendNotify，当前版本: 20220723");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
@@ -128,15 +118,6 @@ let WP_UIDS_ONE = "";
 let GOTIFY_URL = '';
 let GOTIFY_TOKEN = '';
 let GOTIFY_PRIORITY = 0;
-
-/**
- * sendNotify 推送通知功能
- * @param text 通知头
- * @param desp 通知体
- * @param params 某些推送通知方式点击弹窗可跳转, 例：{ url: 'https://abc.com' }
- * @param author 作者仓库等信息  例：`本通知 By：https://github.com/whyour/qinglong`
- * @returns {Promise<unknown>}
- */
 let PushErrorTime = 0;
 let strTitle = "";
 let ShowRemarkType = "1";
@@ -200,9 +181,10 @@ if (process.env.NOTIFY_SHOWNAMETYPE) {
     if (ShowRemarkType == "4")
         console.log("检测到显示备注名称，格式为: 备注");
 }
-async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By https://github.com/Akali5/jd-depot',strsummary="") {
-    console.log(`开始发送通知...`);
-
+async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By ccwav Mod', strsummary = "") {
+    console.log(`开始发送通知...`); 
+	
+	//NOTIFY_FILTERBYFILE代码来自Ca11back.
     if (process.env.NOTIFY_FILTERBYFILE) {
         var no_notify = process.env.NOTIFY_FILTERBYFILE.split('&');
         if (module.parent.filename) {
@@ -585,11 +567,22 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By ht
 		        }
 		    }
 		}
-        
-
-        //console.log("UseGroup2 :"+UseGroup2);
-        //console.log("UseGroup3 :"+UseGroup3);
-
+        if (desp) {
+            for (lncount = 2; lncount < 20; lncount++) {
+                if (process.env["NOTIFY_INCLUDE_TEXT" + lncount]) {
+                    Notify_IncludeText = process.env["NOTIFY_INCLUDE_TEXT" + lncount].split('&');
+                    if (Notify_IncludeText.length > 0) {
+                        for (var Templ in Notify_IncludeText) {
+                            if (desp.indexOf(Notify_IncludeText[Templ]) != -1) {
+                                console.log("检测内容到内容存在组别推送的关键字(" + Notify_IncludeText[Templ] + ")，将推送到组" + lncount + "...");
+                                UseGroupNotify = lncount;
+								break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 		if (UseGroupNotify == 1)
 		    UseGroupNotify = "";
 
@@ -710,7 +703,7 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By ht
                 }
 
                 if (allCode) {
-                    desp += '\n' + '\n' + "格式化后的互助码:" + '\n' + allCode;
+                    desp += '\n' + '\n' + "ccwav格式化后的互助码:" + '\n' + allCode;
                 }
             }
         }
@@ -991,7 +984,7 @@ function getRemark(strRemark) {
     }
 }
 
-async function sendNotifybyWxPucher(text, desp, PtPin, author = '\n\n本通知 By jd-depot仓库', strsummary = "") {
+async function sendNotifybyWxPucher(text, desp, PtPin, author = '\n\n本通知 By ccwav Mod', strsummary = "") {
 
     try {
         var Uid = "";
@@ -1368,7 +1361,7 @@ function tgBotNotify(text, desp) {
           } else {
             data = JSON.parse(data);
             if (data.ok) {
-              console.log('Telegram发送通知消息成功�。\n')
+              console.log('Telegram发送通知消息成功🎉\n')
             } else if (data.error_code === 400) {
               console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n')
             } else if (data.error_code === 401) {
